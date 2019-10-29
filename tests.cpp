b4d6a0e3abf0d9a281faa149df2979108e3aa646
suzukijoe <joe.suzuki02@myhunter.cuny.edu>
2019-10-28 23:46:54 -0400

    Reviews of code

--- tests.cpp
+++ tests.cpp
@@ -9,3 +9,7 @@ TEST_CASE("Test removeLeadingSpaces(string line"){
 TEST_CASE("Test countChar(string line, char c)"){
 	CHECK(countChar("int main(){", '{') == 1);
 }
+
+TEST_CASE("unindent Test"){
+   CHECK(indentedParse("bad-code.cpp") == "int main(){\n\t// Hi, I'm a program!\n\tint x = 1; \n\tfor(int i = 0; i < 10; i++) {\n\t\tcout << i;\n\t\tcout << endl;\n\t}\n}");
+}
